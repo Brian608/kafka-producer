af24e316d80baf9901506e7a401f96567780cf80
feather <Brian608@163.com>
2023-04-24 11:33:20 +0800

    发送消息

---
+++ src/main/java/org/feather/kafka/Application.java
@@ -0,0 +1,21 @@
+package org.feather.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @projectName: kafka-producer
+ * @package: org.feather.kafka
+ * @className: Application
+ * @author: feather(杜雪松)
+ * @description: TODO
+ * @since: 2023-04-24 11:12
+ * @version: 1.0
+ */
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
